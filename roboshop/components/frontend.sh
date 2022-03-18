2187537d3b703940d760cb6776287289b058d098
hari kishan <kishanviit13@gmail.com>
2022-03-18 18:40:07 +0530

    making frontend component

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -12,7 +12,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
   echo -e "\e[31mFAILURE\e[0m"
-    exit 2
+  exit 2
 fi
 
 echo -e "\e[36m Downloading Nginx Content \e[0m"
@@ -21,7 +21,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
   echo -e "\e[31mFAILURE\e[0m"
-    exit 2
+  exit 2
 fi
 
 echo -e "\e[36m Cleanup Old Nginx Content and Extract New downloaded Archive \e[0m"
@@ -39,7 +39,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
   echo -e "\e[31mFAILURE\e[0m"
-    exit 2
+  exit 2
 fi
 
 systemctl enable nginx
