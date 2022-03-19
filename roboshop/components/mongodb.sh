08bc9ac3d2f064af9105a4e58f89e6f882e7522e
hari kishan <kishanviit13@gmail.com>
2022-03-19 21:41:25 +0530

    making Mongodb component setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -30,7 +30,7 @@ yum install -y mongodb-org &>>$LOG_FILE
 STATCHECK $?
 
 Print "Update MongoDB Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 STATCHECK $?
 
 Print "Start MondoDB"
