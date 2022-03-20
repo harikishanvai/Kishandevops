60f1515941ea45fd30124b1a62d0ca2e040ede65
hari kishan <kishanviit13@gmail.com>
2022-03-20 18:38:52 +0530

    Catalogue component setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -37,7 +37,7 @@ chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
 STATCHECK $?
 
 Print "setup systemd file"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
+sed -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
 /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 STATCHECK $?
 
