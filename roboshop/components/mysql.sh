4b3097d42e0d111c1236d00b10633504b396cbd5
hari kishan <kishanviit13@gmail.com>
2022-03-21 21:08:56 +0530

    mysql  setup

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -10,7 +10,10 @@ Print "Install MySQL"
 yum install mysql-community-server -y &>>${LOG_FILE}
 STATCHECK $?
 
+echo "SET PASSWORD FOR 'root@localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
 
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{prnt $NF}' )
+mysql -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
 #1. Start MySQL.
 #
 #```bash
